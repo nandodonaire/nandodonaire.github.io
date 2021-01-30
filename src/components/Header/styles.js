88c5c0d27fce53d36705340da60125871b0a99f6
Fernando Donaire <31811789+nandodonaire@users.noreply.github.com>
2021-01-29 22:58:09 -0500

    Added styled-components for header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,10 +3,16 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  height: 7vh;
+  align-items: center;
+  background-color: #455d92;
 `;
 
-export const NameSection = styled.div``;
+export const NameSection = styled.div`
+  font-size: 1.5rem;
+  color: white;
+`;
 
 export const ButtonSection = styled.div`
-  justify-content: space-between;
+  justify-content: space-around;
 `;
