b991b4dff03bfa60d3ed1173f1e67252cea061de
Fernando Donaire <31811789+nandodonaire@users.noreply.github.com>
2021-01-29 23:11:39 -0500

    Updated Header styling

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,16 +3,18 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 7vh;
+  height: 9vh;
   align-items: center;
   background-color: #455d92;
 `;
 
 export const NameSection = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.75rem;
+  padding: 1rem;
   color: white;
 `;
 
 export const ButtonSection = styled.div`
   justify-content: space-around;
+  padding: 1rem;
 `;
