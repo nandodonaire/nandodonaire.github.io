4f91a1aa1d2f77e2ec58fc07bd1b33077a2d0ac6
Fernando Donaire <31811789+nandodonaire@users.noreply.github.com>
2021-02-27 23:24:55 -0500

    Started adding personal interests

--- src/components/Interests/index.js
+++ src/components/Interests/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SectionContainer } from "../commonStyledComponents/sectionContainer";
 import { SectionTitle } from "../commonStyledComponents/sectionTitle";
+import { SectionSubTitle } from "../commonStyledComponents/sectionSubTitle";
 import { TextContainer } from "../commonStyledComponents/textContainer";
 import { TextSection } from "../commonStyledComponents/textSection";
 
@@ -8,8 +9,16 @@ const Interests = () => (
   <SectionContainer height="50vh" backgroundColor="#c9af98" id="interests">
     <SectionTitle fontColor="#3b4660">Interests</SectionTitle>
     <TextContainer>
-      <TextSection fontColor="#3b4660"></TextSection>
-      <TextSection fontColor="#3b4660"></TextSection>
+      <TextSection fontColor="#3b4660">
+        <SectionSubTitle>Personal Interests</SectionSubTitle>
+        <ul>
+          <li>
+            I love traveling and learning about other cultures. I moved around a
+            lot when I was growing up which I think contributed to my curiosity
+            about how other people live in various places around the world.
+          </li>
+        </ul>
+      </TextSection>
     </TextContainer>
   </SectionContainer>
 );
